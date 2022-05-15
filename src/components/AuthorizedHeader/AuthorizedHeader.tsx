eebc0a99b20ceaa6785f8468289841cf961868af
i-shaban-sloboda <101095965+i-shaban-sloboda@users.noreply.github.com>
2022-05-15 20:30:55 +0300

    add settings menu

--- src/components/AuthorizedHeader/AuthorizedHeader.tsx
+++ src/components/AuthorizedHeader/AuthorizedHeader.tsx
@@ -1,12 +1,39 @@
-import { signOut } from 'next-auth/react'
-import React, { FC, memo } from 'react'
+import { signOut, useSession } from 'next-auth/react'
+import React, { FC, MouseEvent, memo, useCallback, useState } from 'react'
 
 import { pagesPath } from '../../utils/$path'
-import { Button, Stack } from '@mui/material'
+import { Logout, Settings } from '@mui/icons-material'
+import {
+    Avatar,
+    Divider,
+    IconButton,
+    ListItemIcon,
+    Menu,
+    MenuItem,
+    Stack,
+    Tooltip,
+} from '@mui/material'
 
 export interface Props {}
 
 export const AuthorizedHeader: FC<Props> = memo((props) => {
+    const { data } = useSession()
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+    const open = Boolean(anchorEl)
+    const handleClick = useCallback((event: MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget)
+    }, [])
+    const handleClose = useCallback(() => {
+        setAnchorEl(null)
+    }, [])
+    const handleLogout = useCallback(
+        () =>
+            signOut({
+                callbackUrl: pagesPath.login.$url().pathname,
+            }),
+        [],
+    )
+
     return (
         <Stack
             gap={2}
@@ -15,17 +42,73 @@ export const AuthorizedHeader: FC<Props> = memo((props) => {
             justifyContent="end"
             sx={{ p: '16px 32px' }}
         >
-            <Button
-                variant="text"
-                color="secondary"
-                onClick={() =>
-                    signOut({
-                        callbackUrl: pagesPath.login.$url().pathname,
-                    })
-                }
+            Привет {data?.user?.name!}!
+            <Tooltip title="Настройки">
+                <IconButton
+                    onClick={handleClick}
+                    size="small"
+                    aria-controls={open ? 'account-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                >
+                    <Avatar alt={data?.user?.name!} src={data?.user?.image!} />
+                </IconButton>
+            </Tooltip>
+            <Menu
+                anchorEl={anchorEl}
+                id="account-menu"
+                open={open}
+                onClose={handleClose}
+                onClick={handleClose}
+                PaperProps={{
+                    elevation: 0,
+                    sx: {
+                        overflow: 'visible',
+                        filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                        mt: 1.5,
+                        '& .MuiAvatar-root': {
+                            width: 32,
+                            height: 32,
+                            ml: -0.5,
+                            mr: 1,
+                        },
+                        '&:before': {
+                            content: '""',
+                            display: 'block',
+                            position: 'absolute',
+                            top: 0,
+                            right: 14,
+                            width: 10,
+                            height: 10,
+                            bgcolor: 'background.paper',
+                            transform: 'translateY(-50%) rotate(45deg)',
+                            zIndex: 0,
+                        },
+                    },
+                }}
+                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
             >
-                Выйти
-            </Button>
+                <MenuItem>
+                    <Avatar /> Profile
+                </MenuItem>
+                <MenuItem>
+                    <Avatar /> My account
+                </MenuItem>
+                <Divider />
+                <MenuItem>
+                    <ListItemIcon>
+                        <Settings fontSize="small" />
+                    </ListItemIcon>
+                    Настройки
+                </MenuItem>
+                <MenuItem onClick={handleLogout}>
+                    <ListItemIcon>
+                        <Logout fontSize="small" />
+                    </ListItemIcon>
+                    Выйти
+                </MenuItem>
+            </Menu>
         </Stack>
     )
 })
